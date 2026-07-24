08ce509788580972bc3af17b3b6f31d334340ca7
William Dann <wcdann2@gmail.com>
2026-07-24 13:20:25 -0700

    typo

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/williamdann/chess-arena/model
 
 go 1.26.3
-
-require github.com/google/uuid v1.6.0 // indirect
--- src/internal/model/session.go
+++ src/internal/model/session.go
@@ -7,7 +7,7 @@ import (
 
 type Session struct {
 	Id        uuid.UUID `json:"id"`
-	UserId    uuid.UUid `json:"user_id"`
+	UserId    uuid.UUID `json:"user_id"`
 	CreatedAt time.Time `json:"created_at"`
 	ExpiresAt time.Time `json:"expires_at"`
 }
